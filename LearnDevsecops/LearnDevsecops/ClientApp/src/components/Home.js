469f86b6b611ad2e8984eb09c2a53006451cadb8
s1th-s <mail2s1th@gmail.com>
2024-02-24 14:29:39 +0530

    initial commit

---
+++ LearnDevsecops/LearnDevsecops/ClientApp/src/components/Home.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+export class Home extends Component {
+  static displayName = Home.name;
+
+  render() {
+    return (
+      <div>
+        <h1>This is Farm app initial version</h1>
+        <p>built by s1th-s</p>
+      </div>
+    );
+  }
+}
